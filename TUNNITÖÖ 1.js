013d677e651868b3ec63610413176ea3b53315ab
KaurMadisson <90029245+KaurMadisson@users.noreply.github.com>
2021-09-03 12:05:20 +0300

    rahakott,tootehind

---
+++ TUNNITÖÖ 1.js
@@ -0,0 +1,64 @@
+4 ==7
+false
+tootehind = 10
+10
+rahakott = 100
+100
+if (15>17) console.log ("Sul pole piisavalt raha ");
+undefined
+if (rahakott > tootehind) console.log (" sul polep iisavalt raha"), {
+
+}
+VM464:1  sul polep iisavalt raha
+{}
+rahakott = Math.random ()*50
+tootehind= 25
+// if ()
+// else
+25
+if (rahakott > tootehind) console.log (" Sul pole piisavalt raha"); {
+
+}
+undefined
+rahakott = Math.random()*50
+tootehind = 15;
+if (rahakott >=tootehind)
+console.log("Saad osta");
+else
+    console.log("Ei saa osta")
+VM1043:4 Saad osta
+rahakott = Math.random()*50,
+    tootehind = 12;
+id ( rahakott > tootehind) console.log (" Sul on piisavalt raha "); {
+
+}
+VM629:3 Uncaught SyntaxError: Unexpected identifier
+rahakott = Math.random()*50,
+    tootehind = 12;
+if ( rahakott > tootehind) console.log (" Sul on piisavalt raha "); {
+
+}
+VM655:3  Sul on piisavalt raha 
+undefined
+console.log(rahakott)
+VM743:1 24.92805184204432
+undefined
+"Saan osta" + "Ei saa osta"
+'Saan ostaEi saa osta'
+"Sul on rahakotis" + rahakott
+'Sul on rahakotis24.92805184204432'
+rahakott = Math.random()*50
+19.995841871259103
+"Sul on rahakotis" +(rahakott+1)
+'Sul on rahakotis20.995841871259103'
+tootehind = 15;
+if (rahakott >= tootehind)
+console.log("Ei saa osta ");
+VM1490:3 Ei saa osta 
+undefined
+tootehind = 15;
+if (rahakott >= tootehind)
+console.log("Ei saa osta ");
+else
+    CONSOLE.LOG("Ei saa osta ");
+VM1613:3 Ei saa osta 
