3f43cb69030f35dc51a7d4285e1a495b8ad25cb5
KaurMadisson <90029245+KaurMadisson@users.noreply.github.com>
2021-10-08 12:54:38 +0300

    Update tunnitöö.js

--- tunnitöö.js
+++ tunnitöö.js
@@ -1,29 +1,21 @@
-let = mitukordaproovinud =0
-let number = Math.floor(Math.random() * 10) + 1
-console.log(number)
-while (password != "qwe123")
+let Proovinud = 0 
+let vastus = Math.floor(Math.random() * 10) + 1
+console.log(vastus)
+while (password != vastus)
 {
- 
- 
     password = prompt("Sisesta number")
-mitukordaproovinud++
-console.log("on proovinud", mitukordaproovinud)
-if (mitukordaproovinud > 2)
+    Proovinud++
+    console.log('on proovinud', Proovinud )
+    if (Proovinud > 2)
         break
-
-
-if (password!=number)
-alert("Sorry, X ei ole õige number")
-if (password== number-1 || password== number+1)
-alert("Ai, sa pakkusid väga lähedale")
-if (password ==null)
-    break
+    if (password!=vastus)
+    alert("Sorry, X ei ole õige number. ")
+    if (password == vastus -1 || password == vastus +1)
+    alert("Lähedal oled")
+    if (password == null)
+        break 
 
 
 }
-if(password==number) console.log("Yay!Arvasid õige numbri ära!")
-else console.log("ei")
-VM2027:3 4
-VM2027:10 on proovinud 1
-VM2027:10 on proovinud 2
-VM2027:10 on proovinud 3
+if(password==vastus) console.log("Yay! Arvasid õige numbri ära!")
+else console.log("Proovi uuesti")
