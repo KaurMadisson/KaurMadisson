77f11fc716a56f9b449763e48778365cd79ce36c
KaurMadisson <90029245+KaurMadisson@users.noreply.github.com>
2021-11-12 08:50:10 +0200

    Create järjestus.js

---
+++ järjestus.js
@@ -0,0 +1,26 @@
+function descOrder(s) {
+        s.sort().reverse();
+        str1 = s.join("");
+        document.write(str1);
+      }
+ 
+      var s = ")bsndbish/2qvnasdvnjsivsblskbmpcvk";
+      s = s.split("");
+
+    // function call
+      descOrder(s);
+
+ Script
+      function descOrder(s) {
+        s.sort().reverse();
+        str1 = s.join("");
+        document.write(str1);
+      }
+ 
+      var s = ")bsndbish/2qvnasdvnjsivsblskbmpcvk";
+      s = s.split("");
+
+    // function call
+      descOrder(s);
+
+ Script
